ecb2afb4a067c9916f9300d1b95db600e52caf7f
rodrigorhas <rodrigorhas@gmail.com>
2015-08-21 15:55:40 -0300

    Devices and pointer added

--- app.js
+++ app.js
@@ -14,6 +14,16 @@
 			Time: 0,
 		}
 
+		this.config = {
+
+		}
+
+		this.devices = {
+			Mouse : {
+				pointer : new Pointer ()
+			}
+		}
+
 		this.startTime;
 
 		this.loop;
@@ -60,6 +70,19 @@
 
 	}
 
+	Playback.prototype.startRecording = function (deviceName, eventName) {
+		if(!this.config[deviceName]) this.config[deviceName] = {}
+		if(!this.config[deviceName][eventName]) this.config[deviceName][eventName] = {allowRecording: true};
+
+		this.sessionStartTime = Date.now ();
+
+		this._prepareDevice(deviceName, eventName);
+	}
+
+	Playback.prototype.stopRecording = function ( deviceName, eventName ) {
+		this.config[deviceName][eventName].allowRecording = false;
+	}
+
 	Playback.prototype._getEventsTime = function () {
 		var $this = this;
 
@@ -72,15 +95,98 @@
 		})
 	}
 
+	Playback.prototype._prepareDevice = function ( deviceName, eventName ) {
+		var $this = this;
+
+		if(deviceName == 'mouse') {
+			if(eventName == 'mousemove') {
+				window.addEventListener ('mousemove', function () {
+					if($this.config[deviceName][eventName].allowRecording) {
+						$this._OnMouseMove.apply($this, arguments);
+					}
+				}, true);
+			}
+		}
+	}
+
+	//
+	//	Mouse Events Callback
+	//
+
+	Playback.prototype._OnMouseMove = function (event) {
+		var pos = {
+			x : event.x || event.clientX || event.pageX || event.screeX || event.offsetX,
+			y : event.y || event.clientY || event.pageY || event.screeY || event.offsetY
+		}
+
+		var event = {name: 'mousemove', pos: pos};
+
+		this._registerEvent (event);
+	}
+
+	Playback.prototype._registerEvent = function (event) {
+		event.time = Date.now () - this.sessionStartTime;
+		
+		this.events.push(event);
+
+		this._getEventsTime ();
+	}
+
 	Playback.prototype.stop = function () {
 		clearInterval(this.loop);
+	}
+
+	Playback.prototype.replay = function () {
+		var $this = this;
+		var i = -1;
+		var interval = setInterval(function () {
+			
+			var e = $this.events[++i];
+
+			if(i == $this.events.length - 1) {
+				clearInterval(interval)
+			}
+
+			$this.devices.Mouse.pointer.moveTo(e.pos);
+
+		}, 16);
+	}
+
+	function Pointer () {
+		this.dom = document.createElement("span");
+
+		this.dom.style.width  = '10px';
+		this.dom.style.height = '10px';
+		this.dom.style.position = 'absolute';
+		this.dom.style.backgroundColor = 'red';
+
+		document.body.appendChild(this.dom);
+
+		this.moveTo({x: -999, y: -999})
+
+	}
+
+	Pointer.prototype.moveTo = function ( pos ) {
+		this.dom.style.left = pos.x + 'px';
+		this.dom.style.top = pos.y + 'px';
 	}
 
 	window.Playback = Playback;
 
 })();
 
-var pb = new Playback({
+/*var pb = new Playback({
 	events : recordedSession.events,
 	start: true
 });
+*/
+
+var pb = new Playback({events: e});
+
+/*pb.startRecording ('mouse', 'mousemove');
+
+setTimeout (function () {
+	pb.stopRecording ('mouse', 'mousemove');
+
+	console.log(pb.events);
+}, 2000)*/
