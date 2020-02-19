f1626e702d97892c7201d6fa35159601c45636bb
Vinicius Hacebe <vinicius_hacebe@hotmail.com>
2020-02-19 20:47:14 -0300

    JQuery added, json file instead of javascript file, sample data, progressbar, helper functions (secondsToHours, querystring)

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ $('#messages').on("scroll", function(a, b, c){
 function newChatMessage(message, pb){
 	chatMsg(message);
 }
+
 function startVideo(video, pb){
 	var videoEl = $('#videos .row');
 	var videoPlayer = $("<div class='col-sm-4' id=\"" + video.videoId + "\"><div class='card'><video class='card-img-top' autoplay src=\"resources/" + pb.sessionId + "/" + video.videoId + ".webm\"></video><div class='card-body p-1 text-center'><div class='card-text'>"+ video.username +"</div></div></div></div>");
@@ -34,11 +35,43 @@ function startVideo(video, pb){
 
 	chatMsg("<small class='text-muted'><em>"+video.username + " - turned the video on</em></small>");
 }
+
 function stopVideo(video, pb){
 	var videoEl = $('#' + video.videoId);
 	videoEl.remove();
 	chatMsg("<small class='text-muted'><em>"+video.username + " - turned the video off</em></small>");
 }
+
+function secondsToTime(secs)
+{
+    var hours = Math.floor(secs / (60 * 60));
+
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+
+    (hours.toString().length == 1) ? hours = '0'+hours : void 0;    
+    (minutes.toString().length == 1) ? minutes = '0'+minutes : void 0;    
+    (seconds.toString().length == 1) ? seconds = '0'+seconds : void 0; 
+
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
 
 (function ( ) {
 
@@ -52,6 +85,10 @@ function stopVideo(video, pb){
 		this.resources = config.res;
 
 		this.sessionId = config.sessionId;
+		this.sessionLength = config.sessionLength;
+		this.progress = 0;
+
+		this.progressEl = config.progressEl;
 
 		this.allEventsTime = {};
 
@@ -64,6 +101,7 @@ function stopVideo(video, pb){
 		this.startTime;
 
 		this.loop;
+		this.scrubInterval;
 
 		if(config.start && config.start == true)
 			this.init ();
@@ -83,12 +121,21 @@ function stopVideo(video, pb){
 
 		this.startTime = Date.now ();
 
+		this.scrubInterval = setInterval ( function () {
+			$this._updateScrub();
+
+		},100);
+
+		$('#totalTime').html(secondsToTime(this.sessionLength/1000));
+
 		this.loop = setInterval ( function () {
 
 
 			var now = Date.now();
 
 			$this.current.Time = now - $this.startTime;
+
+			$this.progress = ($this.current.Time / $this.sessionLength) * 100;
 
 			var evtTime = $this.allEventsTime[$this.current.Index + 1];
 
@@ -114,7 +161,12 @@ function stopVideo(video, pb){
 
 	}
 
-	
+	Playback.prototype._updateScrub = function(){
+		if(this.progressEl){
+			$(this.progressEl).css("width", this.progress + "%");
+		}
+		$('#currentTime').html(secondsToTime(this.current.Time/1000));
+	}
 
 	Playback.prototype._getEventsTime = function () {
 		var $this = this;
@@ -131,14 +183,42 @@ function stopVideo(video, pb){
 
 	Playback.prototype.stop = function () {
 		clearInterval(this.loop);
+		clearInterval(this.scrubInterval);
 	}
 
 	window.Playback = Playback;
 
 })();
 
-var pb = new Playback({
-	events : recordedSession.events,
-	sessionId: recordedSession.sessionId,
-	start: true
-});
+var session = qs['sID'] ? qs['sID'] : "123456";
+var pb; 
+
+(function(){
+	$.ajax({
+		url: "sessions/" + session + ".json",
+		method: "GET",
+		success: function(data, status, ajx){
+			pb = new Playback({
+				events : data.events,
+				sessionId: data.sessionId,
+				sessionLength: data.sessionLength,
+				progressEl: "#scrubbar",
+				start: false
+			})
+		},
+		error: function(ajx, statusErr, thrownErr){
+			alert('Failed to load session! It might be deleted or just wrong!');
+		}
+	})
+
+	$('#play_btn').click(function(){
+		$("#play_btn").addClass('d-none');
+		$("#stop_btn").removeClass('d-none');
+		pb.init();
+	})
+	$('#stop_btn').click(function(){
+		$("#stop_btn").addClass('d-none');
+		$("#play_btn").removeClass('d-none');
+		pb.stop();
+	})
+})();
